c9df0efccff3a1a2d00a273b67ce8467cd50eef6
ShazaAllam2001 <shazaallam90@gmail.com>
2021-12-25 23:45:46 +0200

    add compose and json schema

--- mail-front/src/app/app.module.ts
+++ mail-front/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { ComposeComponent } from './compose/compose.component';
+import { MailComponent } from './mail/mail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
     SignInComponent,
-    ComposeComponent
+    ComposeComponent,
+    MailComponent
   ],
   imports: [
     BrowserModule,
